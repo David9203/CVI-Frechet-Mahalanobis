80ad0132089c9d8e86e9f66546a982c58f6ca85f
Dvd Rnd <dvdrnd@Dvds-MacBook-Pro.local>
2023-01-23 15:45:44 -0500

    add shparaleltriangular

---
+++ proposal/runparalelcalculateindexgamma.sh
@@ -0,0 +1,26 @@
+#!/bin/bash     
+
+
+pathfile='/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx'
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '0' -e '50'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '50' -e '100'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '100' -e '150'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '150' -e '200'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '200' -e '250'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '250' -e '300' & 
+
+
+
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '300' -e '350'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '350' -e '400'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '400' -e '450'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '450' -e '500'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '500' -e '550'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '550' -e '600'  
+
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '600' -e '650'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '650' -e '700'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '700' -e '750'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '750' -e '800'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '800' -e '850'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_gammaoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutgamma/" -c '850' -e '900' 
---
+++ proposal/runparalelcalculateindexlogistic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash     
+
+
+pathfile='/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx'
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '0' -e '50'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '50' -e '100'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '100' -e '150'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '150' -e '200'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '200' -e '250'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '250' -e '300' & 
+
+
+
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '300' -e '350'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '350' -e '400'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '400' -e '450'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '450' -e '500'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '500' -e '550'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '550' -e '600'  
+
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '600' -e '650'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '650' -e '700'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '700' -e '750'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '750' -e '800'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '800' -e '850'  &
+python3 calculateindex.py  -f '/home/nestord.rendon/nestor/CVI-Frechet-Mahalanobis/AnotherIndicesTest/SyntheticData Configuration/info_generateddata_logisticoutnoise.xlsx' -s "/home/nestord.rendon/nestor/uwindex/noioutlogistic/" -c '850' -e '900' 
